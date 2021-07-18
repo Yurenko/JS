2ec9040b072fe27174680cce67b53fc15b028047
Yurenko <didenkoyura225@gmail.com>
2021-07-18 10:24:35 +0300

    fix

--- homework1/task6/js/index.js
+++ homework1/task6/js/index.js
@@ -3,7 +3,6 @@ let total = 0;
 do {
     input = prompt('Введіть число')
     total = total + Number(input);
-}
-while (input) {
-    alert(`Загальна сума чисел дорівнює ${total}`)
-}
+} while (input)
+
+alert(`Загальна сума чисел дорівнює ${total}`)
